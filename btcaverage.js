e77c9f80a0b6e15ca1bb18ef8ed967291b5f1304
sgsshankar <sgsshankarnarayanan@gmail.com>
2015-12-11 02:02:27 +0530

    Added Unocoin. Added support for multi currency rate with USD base. Added currency to providers.json

--- btcaverage.js
+++ btcaverage.js
@@ -1,6 +1,8 @@
 var Q = require('q');
 var request = require('request');
 var async = require('async');
+var Promise = require("bluebird");
+var getExchangeRates = require("get-exchange-rates-usd");
 var smartaverage = require('smartaverage');
 var providers = require('./providers');
 
@@ -14,15 +16,19 @@ var MINIMUM_VALUES_VARIANCE = 3;
  * @param {Object} jsonData
  * @param {String} path
  */
-function findValueByPath(jsonData, path){
-    var errorParts = false;
-    path.split('.').forEach(function(part){
-        if(!errorParts){
-            jsonData = jsonData[part];
-            if(!jsonData) errorParts = true;
-        }
-    });
-    return errorParts ? 0 : parseFloat(jsonData);
+function findValueByPath(jsonData, path, currency, rate) {
+	var errorParts = false;
+	path.split('.').forEach(function(part) {
+		if (!errorParts) {
+			if (currency) {
+				jsonData = jsonData[part] / rate;
+			} else {
+				jsonData = jsonData[part]
+			}
+			if (!jsonData) errorParts = true;
+		}
+	});
+	return errorParts ? 0 : parseFloat(jsonData);
 }
 
 /**
@@ -31,52 +37,57 @@ function findValueByPath(jsonData, path){
  * @param {String} urlAPI
  * @param {Function} callback
  */
-function requestPrice(urlAPI, callback){
-    request({
-        method: 'GET',
-        url: urlAPI,
-        timeout: TIMEOUT,
-        maxRedirects: 2
-    }, function(error, res, body){
-        if(!error){
-            try{
-                var current = JSON.parse(body);
-                callback(current);
-            }catch(e){}
-        }
-        if(!current) {
-            callback({});
-        }
-    });
+function requestPrice(urlAPI, callback) {
+	request({
+		method: 'GET',
+		url: urlAPI,
+		timeout: TIMEOUT,
+		maxRedirects: 2
+	}, function(error, res, body) {
+		if (!error) {
+			try {
+				var current = JSON.parse(body);
+				callback(current);
+			} catch (e) {}
+		}
+		if (!current) {
+			callback({});
+		}
+	});
 }
+
 
 /**
  * GET PRICE
  */
-module.exports = function getPrice(){
-    var df = Q.defer();
-    async.parallel(providers.map(function(provider){
-            return function(callback){
-                requestPrice(provider.url, function(jsonResponse){
-                    callback(null, findValueByPath(jsonResponse, provider.path));
-                });
-            }
-        }),
-        function(err, prices){
-            var infoAverage = smartaverage(ACCEPTABLE_VARIANCE, MINIMUM_VALUES_VARIANCE, prices);
-            if(infoAverage){
-                var pricesProviders = {};
-                prices.forEach(function(price, i){
-                    pricesProviders[providers[i].name] = price;
-                });
-                df.resolve({
-                    average: infoAverage.average,
-                    pricesAverage: infoAverage.dataset,
-                    prices: pricesProviders
-                });
-            }else{
-                df.reject(new Error('Was imposible get price average'));
-            }
-        });
-    return df.promise;
+module.exports = function getPrice() {
+	var df = Q.defer();
+	Promise.try(function() {
+		return getExchangeRates();
+	}).then(function(rates) {
+		async.parallel(providers.map(function(provider) {
+				return function(callback) {
+					requestPrice(provider.url, function(jsonResponse) {
+						callback(null, findValueByPath(jsonResponse, provider.path, provider.currency, rates[provider.currency]));
+					});
+				}
+			}),
+			function(err, prices) {
+				var infoAverage = smartaverage(ACCEPTABLE_VARIANCE, MINIMUM_VALUES_VARIANCE, prices);
+				if (infoAverage) {
+					var pricesProviders = {};
+					prices.forEach(function(price, i) {
+						pricesProviders[providers[i].name] = price;
+					});
+					df.resolve({
+						average: infoAverage.average,
+						pricesAverage: infoAverage.dataset,
+						prices: pricesProviders
+					});
+				} else {
+					df.reject(new Error('Was imposible get price average'));
+				}
+			});
+	});
+	return df.promise;
 };
